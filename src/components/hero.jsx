aa8c6c4c1b6625961c4bd21b8980bbddcdfc0cd0
Donmane <mdon85329@gmail.com>
2026-08-07 01:27:59 -0700

    Add Hero component

--- src/components/hero.jsx
+++ src/components/hero.jsx
@@ -1,8 +1,15 @@
-
 function Hero() {
   return (
-    <div>Hero</div>
-  )
+    <div>
+      <h1>Edith-Agoye Daniel Olamide</h1>
+      <p>Frontend Developer · Building for the web</p>
+      <p>I build things that work.</p>
+      <div>
+        <button>Contact Me</button>
+        <button>View my Projects</button>
+      </div>
+    </div>
+  );
 }
 
-export default Hero
+export default Hero;
