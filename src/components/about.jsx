2d34e0f2b25dec3ba6646d9b1f4433788d4f8b72
Donmane <mdon85329@gmail.com>
2026-08-07 01:39:39 -0700

    Add About component

--- src/components/about.jsx
+++ src/components/about.jsx
@@ -1,7 +1,10 @@
 
 function About() {
   return (
-    <div>About</div>
+    <div>
+      <h2>About Me</h2>
+      <p>I am a Frontend Developer and a Software Engineering student at Lead City University Ibadan, I build awesome apps with React, Javascript, Tailwind, and Supabase from scratch to production with Vercel. I'm passionate about technology and contributing to what's next, and I will stop at nothing to achieve my goals.</p>
+    </div>
   )
 }
 
