284d68df3667f93984116622c620ca2bdf26fe39
Donmane <mdon85329@gmail.com>
2026-08-08 05:10:23 -0700

    Add Skills section with AnimatedList and devicons

--- src/components/skills.jsx
+++ src/components/skills.jsx
@@ -1,8 +1,29 @@
-import React from 'react'
+import AnimatedList from './AnimatedList'
+
+const items = [
+  'HTML', 
+  'CSS5', 
+  'JavaScript', 
+  'React', 
+  'Tailwind CSS', 
+  'Supabase', 
+  'Git', 
+  'Vercel',
+  'GSAP',
+]
 
 function Skills() {
   return (
-    <div>Skills</div>
+    <div>
+      <h2>Skills</h2>
+      <AnimatedList
+        items={items}
+        onItemSelect={(item, index) => console.log(item, index)}
+        showGradients
+        enableArrowNavigation
+        displayScrollbar
+      />
+    </div>
   )
 }
 
