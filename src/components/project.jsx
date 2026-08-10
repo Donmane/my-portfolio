ba42c2d8afa96e655edaeaadaded670a4e1e4331
Donmane <mdon85329@gmail.com>
2026-08-10 02:46:26 -0700

    Add Projects section with TiltedCard component

--- src/components/project.jsx
+++ src/components/project.jsx
@@ -1,8 +1,55 @@
-import React from 'react'
-
+import TiltedCard from './TiltedCard'
 function Project() {
   return (
-    <div>Project</div>
+  <>
+  <div>
+
+  <div>
+    <TiltedCard
+  imageSrc="/src/assets/peerup.png"
+  altText="PeerUp"
+  captionText="PeerUp"
+  containerHeight="300px"
+  containerWidth="300px"
+  imageHeight="300px"
+  imageWidth="300px"
+  rotateAmplitude={12}
+  scaleOnHover={1.05}
+  showTooltip
+/>
+<h3>PeeUp</h3>
+<p>A student study help notice board where you can post what you're stuck on and connect with peers via email.</p>
+<a href="https://peer-up-eight.vercel.app/">Live project</a>
+<a href="https://github.com/Donmane/PeerUp">GitHub</a>
+</div>
+
+
+  <div>
+    <TiltedCard
+  imageSrc="/src/assets/properaetherbook.png"
+  altText="PeerUp"
+  captionText="PeerUp"
+  containerHeight="300px"
+  containerWidth="300px"
+  imageHeight="300px"
+  imageWidth="300px"
+  rotateAmplitude={12}
+  scaleOnHover={1.05}
+  showTooltip
+/>
+<h3>PeeUp</h3>
+<p>A student study help notice board where you can post what you're stuck on and connect with peers via email.</p>
+<a href="https://peer-up-eight.vercel.app/">Live project</a>
+<a href="https://github.com/Donmane/PeerUp">GitHub</a>
+</div>
+
+
+
+  </div>
+
+</>
+
+
   )
 }
 
